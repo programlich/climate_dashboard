2fbaa0111dcae93852f6477b1a79bb9e74e095bb
Matthias <matthias@MXThinkpad>
2022-11-28 17:13:46 +0100

    Removed dead code

--- Analysis/app.py
+++ Analysis/app.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
-import os
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 
@@ -39,33 +38,11 @@ fig_temp_bar["layout"].pop("updatemenus")
 
 # Import emission data per country
 emissions_country = pd.read_csv("/home/matthias/Python/Klimadashboard/Analysis/data/emissions_country.csv")
-#emissions_country = pd.read_excel("/home/matthias/Python/Klimadashboard/Daten/EDGARv7.0_FT2021_fossil_CO2_booklet_2022.xlsx",
-#                                 sheet_name="fossil_CO2_totals_by_country").dropna()
 
 # Import emission data per capita
 emissions_capita = pd.read_csv("/home/matthias/Python/Klimadashboard/Analysis/data/emissions_country.csv")
-#emissions_capita = pd.read_excel("/home/matthias/Python/Klimadashboard/Daten/EDGARv7.0_FT2021_fossil_CO2_booklet_2022.xlsx",
-#                                 sheet_name="fossil_CO2_per_capita_by_countr",skipfooter=3).dropna()
 
-""" # Create inverted df for country emissions
-emissions_country_inverted = emissions_country.drop(emissions_country.tail(1).index).drop("Substance",axis=1).melt(id_vars=["Country","EDGAR Country Code"],var_name="year",value_name="emissions")
-# Create inverted df for per capita emissions
-emissions_capita_inverted = emissions_capita.drop(emissions_capita.tail(1).index).drop("Substance",axis=1).melt(id_vars=["EDGAR Country Code","Country"], var_name="year",value_name="emissions")
-# Merge the two dfs
-emissions_inverted_merged = pd.merge(left=emissions_country_inverted, right=emissions_capita_inverted, on=["EDGAR Country Code","Country","year"],suffixes=["_country","_capita"])
-# Convert country emissions from Mt to Gt
-emissions_inverted_merged["emissions_country"] = emissions_inverted_merged["emissions_country"] / 1000
-
-# Function for calculating the cumulated emissions of a country
-def calc_cumulated_emissions(row):
-    year = row["year"]
-    country = row["Country"]
-
-    cumulated_emissions = emissions_inverted_merged.loc[(emissions_inverted_merged["year"]<=year) & (emissions_inverted_merged["Country"]==country),"emissions_country"].sum()
-    
-    return cumulated_emissions
-emissions_inverted_merged["emissions_cumulated"] = emissions_inverted_merged.apply(calc_cumulated_emissions, axis=1) """
-
+#Import inverted and merged emissions
 emissions_inverted_merged = pd.read_csv("/home/matthias/Python/Klimadashboard/Analysis/data/emissions_inverted_merged.csv")
 
 # Create a dict list of all countries for the dropdown menu
@@ -136,72 +113,6 @@ app.layout = dbc.Container([
     
                 ])
 
-
-
-""" app.layout = html.Div(children=[
-    
-    # Header
-    html.H1("Klimadashboard",
-        style={'display':'block', "text-align":"center"}),
-    
-        #First row
-        html.Div([
-            # Subheader
-            html.H2("Änderung der globalen Oberflächentemperatur relativ zum Zeitraum 1850-1900",
-                style={"text-align":"center"}),
-            
-            #Temp line graph
-            html.Div(dcc.Graph(figure=fig_temp_line),
-                style={"display":"inline-block","margin":"10px auto",
-                        "width":"70%"}),
-            
-            # Bar temp graph    
-            html.Div(dcc.Graph(figure=fig_temp_bar), style={"display":"inline-block", "width":"30%", "margin":"10px auto"})],
-            # Styling first row
-                style={'width':'90%','margin':"10px auto", 
-                            "border":"2px solid green"}
-                ), #Close first row
-        
-
-        # Second row
-        html.Div([
-            
-            # Header second row
-            html.H2("CO2 Emissionen durch Verbrennung fossiler Energieträger",style={"text-align":"center"}),
-            # Country dropdown
-            html.Div(daq.BooleanSwitch(id="cummulation_switch",
-                            on=False,
-                            label="Kummulierte Emissionen",
-                            labelPosition="right",
-                            style={"width":"25%","margin":"auto",
-                                "display":"block"}
-                            )
-                    ),
-            
-            html.Div(
-                dcc.Dropdown(id="country_dd",
-                        options=dict_list_countries,
-                        multi=True,
-                        style={"width":"45%","margin":"10px auto"}) 
-                    ), 
-                       
-
-            # Emissions country/capita plot
-            html.Div(dcc.Graph(id="fig_country_capita"),
-                style={"width":"90%","margin":"10px auto",
-                        "display":"block"})
-
-
-                ], #closing second row children
-
-            # Styling second row
-            style={'width':'90%','margin':"10px auto", 
-                            "border":"2px solid green"}
-
-                )
-
-        ])  # Close global div """
-                           
       
 @app.callback(
     Output(component_id="fig_country_capita",component_property="figure"),
